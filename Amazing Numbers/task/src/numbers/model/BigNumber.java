b214a4816274d91dac2abb5afea8d75e71d9e1b5
Anna Terehova <anna.terehova@gmail.com>
2021-08-11 21:36:57 +0300

    Fixed a bug in BigNumber::isNatural

--- Amazing Numbers/task/src/numbers/model/BigNumber.java
+++ Amazing Numbers/task/src/numbers/model/BigNumber.java
@@ -7,7 +7,7 @@ import java.util.function.Function;
 import java.util.regex.Pattern;
 
 public class BigNumber extends BigInteger {
-    public static final Pattern NATURAL = Pattern.compile("[+]?\\d*[1-9]");
+    public static final Pattern NATURAL = Pattern.compile("[+]?\\d*\\d");
     public static final Map<String, Function<BigNumber, Boolean>> numberProperties = new HashMap<>();
 
     private final Map<String, Boolean> properties = new HashMap<>();
@@ -26,10 +26,6 @@ public class BigNumber extends BigInteger {
 
     public BigNumber(String val) {
         super(val);
-    }
-
-    public boolean isNatural() {
-        return this.compareTo(ZERO) > 0;
     }
 
     public boolean hasProperty(String name) {
