27079e9e1aab09d83916d90433ce355907a166f0
Anna Terehova <anna.terehova@gmail.com>
2021-08-11 21:34:59 +0300

    Implementation of stage 3 (has an error)

--- Amazing Numbers/task/src/numbers/model/BigNumber.java
+++ Amazing Numbers/task/src/numbers/model/BigNumber.java
@@ -1,16 +1,23 @@
-package numbers;
+package numbers.model;
 
 import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 
 public class BigNumber extends BigInteger {
+    public static final Pattern NATURAL = Pattern.compile("[+]?\\d*[1-9]");
     public static final Map<String, Function<BigNumber, Boolean>> numberProperties = new HashMap<>();
+
     private final Map<String, Boolean> properties = new HashMap<>();
 
     public static void addProperty(String name, Function<BigNumber, Boolean> property) {
         numberProperties.put(name.toLowerCase(), property);
+    }
+
+    public static boolean isNatural(final String value) {
+        return NATURAL.matcher(value).matches();
     }
 
     public static boolean isValidProperty(String name) {
