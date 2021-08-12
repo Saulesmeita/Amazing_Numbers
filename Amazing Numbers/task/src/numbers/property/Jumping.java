19b780c3c29c6cccc863ad85e40780fd0d11e95b
Anna Terehova <anna.terehova@gmail.com>
2021-08-12 08:29:44 +0300

    Add implementation for Jumping

--- Amazing Numbers/task/src/numbers/property/Jumping.java
+++ Amazing Numbers/task/src/numbers/property/Jumping.java
@@ -6,6 +6,17 @@ import java.util.function.Predicate;
 public class Jumping implements Predicate<BigInteger> {
     @Override
     public boolean test(BigInteger bigInteger) {
-        return false;
+        var previous = bigInteger.mod(BigInteger.TEN);
+        var rest = bigInteger.divide(BigInteger.TEN);
+        while (rest.compareTo(BigInteger.ZERO) > 0) {
+            var current = rest.mod(BigInteger.TEN);
+            var difference = current.subtract(previous).abs();
+            if (difference.compareTo(BigInteger.ONE) != 0) {
+                return false;
+            }
+            previous = current;
+            rest = rest.divide(BigInteger.TEN);
+        }
+        return true;
     }
 }
